57724cd8bfc4b8d87c6aa990da689eaf20ab0252
HarshVSingla <imharshsingla@gmail.com>
2026-08-22 18:48:27 +0530

    Working Trip System

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -29,6 +29,67 @@ if (profileAvatar) {
 
 }
 
+// ==========================================
+// LOAD SAVED TRIPS
+// ==========================================
+
+const tripGrid = document.getElementById("tripGrid");
+
+const trips =
+    JSON.parse(localStorage.getItem("offwegoTrips")) || [];
+
+
+// GENERATING THE CARDS FOR TRIPS CREATED
+
+trips.forEach(function (trip) {
+
+    const card = document.createElement("a");
+
+    card.classList.add("trip-card");
+
+    card.href = "#";
+
+    card.innerHTML = `
+        <div class="trip-image">
+
+            <img
+                src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?auto=format&fit=crop&w=700&q=85"
+                alt="${trip.destination}"
+            >
+
+            <span class="trip-status">
+                Upcoming
+            </span>
+
+        </div>
+
+        <div class="trip-info">
+
+            <h3>${trip.destination}</h3>
+
+            <p class="trip-date">
+                ${trip.startDate} - ${trip.endDate}
+            </p>
+
+            <div class="trip-bottom">
+
+                <span>
+                ♟ ${trip.adults} Adults · ${trip.children} Children
+                </span>
+
+                <strong>
+                    ${trip.budget || "Budget not set"}
+                </strong>
+
+            </div>
+
+        </div>
+    `;
+
+    tripGrid.appendChild(card);
+
+});
+
 // NEW TRIP BUTTON
 
 const newTripBtn = document.getElementById("newTripBtn");
